63f2cc458bbeceed100748f2d76379597379ab3f
Paulo Araujo <pauloaraujo.phz@gmail.com>
2023-07-18 04:06:02 -0300

    fix: ajustes no template

--- components/Nav.js
+++ components/Nav.js
@@ -1,17 +1,26 @@
+import {
+  HiChatBubbleBottomCenterText,
+  HiEnvelope,
+  HiHome,
+  HiRectangleGroup,
+  HiUser,
+  HiViewColumns,
+} from "react-icons/hi2";
+
 //  links
 const links = [
-  { name: 'home', path: '/', icon: <HiHome /> },
-  { name: 'about', path: '/about', icon: <HiUser /> },
-  { name: 'services', path: '/services', icon: <HiRectangleGroup /> },
-  { name: 'work', path: '/work', icon: <HiViewColumns /> },
+  { name: "home", path: "/", icon: <HiHome /> },
+  { name: "about", path: "/about", icon: <HiUser /> },
+  { name: "services", path: "/services", icon: <HiRectangleGroup /> },
+  { name: "work", path: "/work", icon: <HiViewColumns /> },
   {
-    name: 'testimonials',
-    path: '/testimonials',
+    name: "testimonials",
+    path: "/testimonials",
     icon: <HiChatBubbleBottomCenterText />,
   },
   {
-    name: 'contact',
-    path: '/contact',
+    name: "contact",
+    path: "/contact",
     icon: <HiEnvelope />,
   },
 ];
